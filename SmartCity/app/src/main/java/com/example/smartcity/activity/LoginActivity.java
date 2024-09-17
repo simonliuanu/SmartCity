b0d6b184343840823dce538a42371aff9a8ef99f
Shengzong Dai <u7811526@anu.edu.au>
2024-09-17 19:00:45 +1000

    Build login function basic structure

--- SmartCity/app/src/main/java/com/example/smartcity/activity/LoginActivity.java
+++ SmartCity/app/src/main/java/com/example/smartcity/activity/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.smartcity.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +14,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.smartcity.R;
 
+/**
+ * This activity
+ */
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginAccount;
+    private EditText loginPwd;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +33,56 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loginAccount = findViewById(R.id.login_account);
+        loginPwd = findViewById(R.id.login_pwd);
+        btnLogin = findViewById(R.id.login_btn);
+        btnLogin.setOnClickListener(v -> {
+            String account = loginAccount.getText().toString().trim();
+            String pwd = loginPwd.getText().toString().trim();
+            login(account,pwd);
+        });
+    }
+
+    /**
+     * used to implement user login function
+     * @param account user's account
+     * @param pwd user's password
+     * @author Shengzong Dai
+     * @uid u7811526
+     */
+    private void login(String account, String pwd) {
+        // if the account or password is null, this user won't be allowed to login
+        if(account.isEmpty()) {
+            Toast.makeText(this,"Account can't be null",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(pwd.isEmpty()) {
+            Toast.makeText(this,"Password can't be null",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // jump to main page
+        // TODO hard code, need to refine later
+        if(userExist(account,pwd)) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this,"Account or password wrong",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * used to check if the user exist
+     * @param account user's account
+     * @param pwd user's password
+     * @author Shengzong Dai
+     * @uid u7811526
+     */
+    private boolean userExist(String account, String pwd) {
+        // TODO need refine when implement database
+        if(account.equals("comp2100@anu.edu.au") && pwd.equals("comp2100")
+         || account.equals("comp6442@anu.edu.au") && pwd.equals("comp6442")) return true;
+        return false;
     }
 }
