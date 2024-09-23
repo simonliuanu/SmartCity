68d0234181372b20880375758671e846db4828f6
Tianfa Zhu <u7615711@anu.edu.au>
2024-09-23 19:52:01 +1000

    Finish basic main pages framework

--- SmartCity/app/src/main/java/com/example/smartcity/activity/MainActivity.java
+++ SmartCity/app/src/main/java/com/example/smartcity/activity/MainActivity.java
@@ -1,26 +1,70 @@
 package com.example.smartcity.activity;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.smartcity.R;
+import com.example.smartcity.fragment.HomeFragment;
+import com.example.smartcity.fragment.MapFragment;
+import com.example.smartcity.fragment.ItemFragment;
+import com.example.smartcity.fragment.MeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Set WindowInsets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Get BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navi_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+
+                case R.id.navi_map:
+                    selectedFragment = new MapFragment();
+                    break;
+
+                case R.id.navi_item:
+                    selectedFragment = new ItemFragment();
+                    break;
+
+                case R.id.navi_me:
+                    selectedFragment = new MeFragment();
+                    break;
+
+                default:
+                    return false;
+            }
+
+            // Replace the current Fragment
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_fragment, selectedFragment)
+                    .commit();
+            return true;
+        });
+
+        // Set the default selected Fragment
+        if (savedInstanceState == null) {
+            bottomNavigationView.setSelectedItemId(R.id.navi_home); // Default to HomeFragment
+        }
     }
 }
---
+++ SmartCity/app/src/main/java/com/example/smartcity/fragment/MapFragment.java
@@ -0,0 +1,19 @@
+package com.example.smartcity.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.smartcity.R;
+
+public class MapFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
