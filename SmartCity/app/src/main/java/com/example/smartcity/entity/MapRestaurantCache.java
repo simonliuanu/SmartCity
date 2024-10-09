1f5ce594bf2fede3212de18625c424d179a0796c
Rongze Gao <u7841935@anu.edu.au>
2024-10-09 22:30:53 +1100

    Implemented the query for nearby restaurants on the map.

--- SmartCity/app/src/main/java/com/example/smartcity/entity/MapRestaurantCache.java
+++ SmartCity/app/src/main/java/com/example/smartcity/entity/MapRestaurantCache.java
@@ -6,15 +6,13 @@ import java.util.List;
 
 // This cache class is used for the subsequent loading of restaurants near the current location
 public class MapRestaurantCache {
-    private static MapRestaurantCache instance; // 单例实例
+    private static MapRestaurantCache instance;
     private HashMap<String, List<MarkerOptions>> cachedRestaurants;
 
-    // 私有构造函数
     private MapRestaurantCache() {
         cachedRestaurants = new HashMap<>();
     }
 
-    // 获取单例实例
     public static MapRestaurantCache getInstance() {
         if (instance == null) {
             instance = new MapRestaurantCache();
@@ -22,7 +20,6 @@ public class MapRestaurantCache {
         return instance;
     }
 
-    // 获取缓存的餐馆
     public HashMap<String, List<MarkerOptions>> getCachedRestaurants() {
         return cachedRestaurants;
     }
