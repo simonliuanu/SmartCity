12b9e899f72395c15ba2a45dbc4cce1d514562a1
Shengzong Dai <u7811526@anu.edu.au>
2024-10-16 12:27:40 +1100

    Update javadoc of Observer pattern related classes

--- SmartCity/app/src/main/java/com/example/smartcity/backend/observer/Subject.java
+++ SmartCity/app/src/main/java/com/example/smartcity/backend/observer/Subject.java
@@ -1,9 +1,24 @@
 package com.example.smartcity.backend.observer;
 
+/**
+ * author: Shengzong Dai (u7811526)
+ */
 public interface Subject {
+
+    /**
+     * Attach an observer
+     * @param observer
+     */
     void attach(LikeRestaurantObserver observer);
 
+    /**
+     * Detach an observer
+     * @param observer
+     */
     void detach(LikeRestaurantObserver observer);
 
+    /**
+     * Notify all attached observers of a change in the subject's state.
+     */
     void notifyAllObservers();
 }
